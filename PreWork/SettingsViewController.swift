a5437a35c9feba45b96f9b05b6f2b030b133f890
Anthony <anthony.pena0898@gmail.com>
2021-05-18 12:29:22 -0400

    Adds slider and event handling

---
+++ PreWork/SettingsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsViewController.swift
+//  PreWork
+//
+//  Created by Anthony Pena on 5/17/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBOutlet weak var tipSliderSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func tipSliderSwitched(_ sender: Any) {
+        // Enable the slider and disable the selector
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PreWork/ViewController.swift
+++ PreWork/ViewController.swift
@@ -12,14 +12,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
+        billAmountTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        tipSlider.setValue(15, animated: false)
     }
 
 
     @IBAction func calculateTip(_ sender: Any) {
+        if(tipControl.selectedSegmentIndex == 3){
+            onSliderChangedWithSlider(tipSlider)
+            return
+        }
+        
         // Get the bill amount
         let bill = Double(billAmountTextField.text!) ?? 0
         
@@ -27,11 +36,33 @@ class ViewController: UIViewController {
         let tipPercentages = [0.15, 0.18, 0.20]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
+        tipSlider.setValue(Float(100 * tipPercentages[tipControl.selectedSegmentIndex]), animated: true)
         
         // Update the amount label and the total
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
     
+    
+    @IBAction func onSliderChangedWithSlider(_ sender: UISlider) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let tipPercentage = Double(tipSlider.value) / 100
+        
+        let tip = bill * tipPercentage
+        
+        let total = bill + tip
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func textFieldDidChange(_ sender: UITextField){
+        calculateTip(sender)
+    }
+    
+    @IBAction func sliderTouched(_ sender: Any) {
+        tipControl.selectedSegmentIndex = 3
+    }
 }
 
